a1b7ab3c854a815506c862d891337cfdc5c8cd3e
CodeFFun <sharmasanketa899@gmail.com>
2025-02-05 18:52:52 +0545

    Some minor fixes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRouter = require('./user.js');
-const authRouter = require('./Auth.js');
+const authRouter = require('./auth.js');
 const reviewRouter = require('./review.js'); 
 const ticketRouter = require('./ticket.js');
 const eventRouter = require('./event.js'); 
@@ -11,8 +11,6 @@ const router = express.Router();
 router.use('/auth', authRouter)
 
 router.use(verifyCookie);
-
-
 router.use('/user', userRouter)
 router.use('/review', reviewRouter)
 router.use('/ticket', ticketRouter)
